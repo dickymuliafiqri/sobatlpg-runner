756c330cf1ba9c2515efae7d7937a73a47f1e01b
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2026-02-27 08:48:32 +0700

    update installation script and service file to include environment file configuration

--- script/install.sh
+++ script/install.sh
@@ -5,6 +5,9 @@ PROJECT_DIR=$DIR/..
 SERVICE_DIR=/etc/systemd/system
 WORKING_DIR=/usr/local/etc/sobatlpg
 
+sudo apt-get update
+sudo apt-get install -y libatk1.0-0 libatk-bridge2.0-0 libgtk-3.0 libasound2 libgbm1
+
 sudo rm -rf $SERVICE_DIR/sobatlpg.service
 sudo cp $PROJECT_DIR/script/sobatlpg.service $SERVICE_DIR/
 
