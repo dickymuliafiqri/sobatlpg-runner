5805130d8c88181e9f8966fa12780479d6113885
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2026-02-27 10:03:22 +0700

    update installation script to ensure golang-go installation and correct directory navigation

--- script/install.sh
+++ script/install.sh
@@ -5,8 +5,9 @@ PROJECT_DIR=$DIR/..
 SERVICE_DIR=/etc/systemd/system
 WORKING_DIR=/usr/local/etc/sobatlpg
 
+sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt-get update
-sudo apt-get install -y golang libatk1.0-0 libatk-bridge2.0-0 libgtk-3.0 libasound2 libgbm1
+sudo apt-get install -y golang-go libatk1.0-0 libatk-bridge2.0-0 libgtk-3.0 libasound2 libgbm1
 
 sudo rm -rf $SERVICE_DIR/sobatlpg.service
 sudo cp $PROJECT_DIR/script/sobatlpg.service $SERVICE_DIR/
@@ -15,6 +16,7 @@ sudo rm -rf $WORKING_DIR
 sudo mkdir -p $WORKING_DIR
 
 git clone https://github.com/dickymuliafiqri/sobatlpg
+cd sobatlpg
 go build -o $WORKING_DIR/sobatlpg ./sobatlpg/cmd/sobatlpg/main.go
 
 sudo systemctl daemon-reload
